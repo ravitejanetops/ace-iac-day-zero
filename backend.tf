da04ef01b75bcae78d3b4683bccea77d4045b377
ravitejanetops <114049605+ravitejanetops@users.noreply.github.com>
2022-09-21 14:29:13 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -2,8 +2,7 @@ terraform {
   required_version = ">= 1.1.0"
   backend "remote" {
     hostname = "app.terraform.io"
-    # organization = "Aviatrix-Ravi
-"
+    # organization = "Aviatrix-Ravi"
     workspaces {
       name = "ace-iac-day-zero"
     }
